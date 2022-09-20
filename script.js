62d790f46e55a7a24317f2f4c27975cbce8ed25a
Sanjay <110934351+SANJAY-MECHANICAL@users.noreply.github.com>
2022-09-20 13:43:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const mypokemon = document.getElementById('pokemon');
+
+const fetchPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 60; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((result) => ({
+            name: result.name,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join(', '),
+            weight:result.weight,
+            id: result.id
+        }));
+        displayPokemon(pokemon);
+    });
+};
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon
+        .map(
+            (pokeman) => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+            <p class="card-subtitle">weight: ${pokeman.weight}</p>
+        </li>
+    `
+        )
+        .join('');
+    mypokemon.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
